7fcc513d369a707354cc390dce9605ec50bde0d7
Adam Deen <adamdeen@hotmail.co.uk>
2023-03-13 13:18:56 +0000

    initialised react app, added tailwindcss and declared styling template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex mx-auto justify-center py-7">
+        <span className="text-6xl font-playfair font-bold z-10 text-center md:text-start">
+          Wha<span className=" text-blue">Todo</span>
+        </span>
+      </div>
     </div>
   );
 }
